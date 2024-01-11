981d96783aa9b715a27efdee967aa31dd2f1fdf9
shashimadgi <145380337+shashimadgi@users.noreply.github.com>
2024-01-11 17:15:43 +0530

    Update test_login.py

--- test_login.py
+++ test_login.py
@@ -141,7 +141,7 @@ def test_select_game_validation(game_id):
         'Platform': 'android',
         'SID': '1689061440422-74140',
         'TS': 'undefined',
-        'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiZ21pUHJvZmlsZUlkIjo0Mzc5MjksImV4cCI6MTcwNzU2NTIxNSwiZnJlZWZpcmVQcm9maWxlSWQiOjQzNzkzOCwiaWF0IjoxNzA0OTczMjE1LCJpZCI6NTA5NjIxfQ.Uebx8VqqZNxIIB007DAPFHWLeSPzrYz9YNTQ45eos24'
+        'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiZ21pUHJvZmlsZUlkIjo0Mzc5MjksImV4cCI6MTcwNzU2NTIxNSwiZnJlZWZpcmVQcm9maWxlSWQiOjQzNzkzOCwiaWF0IjoxNzA0OTczMjE1LCJpZCI6NTA5NjIxfQ.Uebx8VqqZNxIIB007DAPFHWLeSPzrYz9YNTQ45eos24'
         # 'Authorization': f'Bearer {access_token}'
     }
     base_url = 'https://api.getstan.app'
