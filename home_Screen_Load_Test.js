918baf8825488403ac97efefa0f20af87195a25c
shashimadgi <145380337+shashimadgi@users.noreply.github.com>
2024-02-07 18:47:13 +0530

    Update home_Screen_Load_Test.js

--- home_Screen_Load_Test.js
+++ home_Screen_Load_Test.js
@@ -1,9 +1,8 @@
-
 import http from 'k6/http';
 import { check } from 'k6';
 
 export const options = {
-    vus: 5,
+    vus: 50,
     duration: '10s',
     thresholds: {
         checks: ['rate==1.00'], // Ensure 100% of checks must pass
@@ -12,24 +11,24 @@ export const options = {
 
 export default function () {
     const urls = [
-        "https://stage-api.getstan.app/api/v3/user/onboarding/status",
-        "https://stage-api.getstan.app/api/v1/users/profile/percentage",
-        "https://stage-api.getstan.app/api/v3/user/gamecurrency",
-        "https://stage-api.getstan.app/api/v1/user/offers/stats",
-        "https://stage-api.getstan.app/api/v3/user/coins",
-        "https://stage-api.getstan.app/api/v3/app/carousels/home",
-        "https://stage-api.getstan.app/api/v3/user/rewards/status",
-        "https://stage-api.getstan.app/api/v4/home/games", 
-        "https://stage-api.getstan.app/api/v3/user/carousels/levelup",
-        "https://stage-api.getstan.app/api/v4/fetch/streak/status", 
-        "https://stage-api.getstan.app/api/v2/app/championships",
-        "https://stage-api.getstan.app/api/v4/home/marketwatch-and-winners", 
-        "https://stage-api.getstan.app/api/v1/articles/suggested",
-        "https://stage-api.getstan.app/api/v1/user/inapp/messages",
-        "https://stage-api.getstan.app/engagement/api/v1/events",
-        "https://stage-api.getstan.app/api/v2/users/missions/info",
-        "https://stage-api.getstan.app/api/v1/agora/channels",
-        "https://stage-api.getstan.app/api/v1/users/update-profile"
+        "https://api.getstan.app/api/v3/user/onboarding/status",
+        "https://api.getstan.app/api/v1/users/profile/percentage",
+        "https://api.getstan.app/api/v3/user/gamecurrency",
+        "https://api.getstan.app/api/v1/user/offers/stats",
+        "https://api.getstan.app/api/v3/user/coins",
+        "https://api.getstan.app/api/v3/app/carousels/home",
+        "https://api.getstan.app/api/v3/user/rewards/status",
+        "https://api.getstan.app/api/v4/home/games", 
+        "https://api.getstan.app/api/v3/user/carousels/levelup",
+        "https://api.getstan.app/api/v4/fetch/streak/status", 
+        "https://api.getstan.app/api/v2/app/championships",
+        "https://api.getstan.app/api/v4/home/marketwatch-and-winners", 
+        "https://api.getstan.app/api/v1/articles/suggested",
+        "https://api.getstan.app/api/v1/user/inapp/messages",
+        "https://api.getstan.app/engagement/api/v1/events",
+        "https://api.getstan.app/api/v2/users/missions/info",
+        "https://api.getstan.app/api/v1/agora/channels",
+        "https://api.getstan.app/api/v1/users/update-profile"
         
     ];
 
@@ -37,17 +36,21 @@ export default function () {
         headers: {
             "Accept": "application/json, text/plain, */*",
             "gaid": "9af1c2ff-4a28-4032-ae0c-b256b9258fe8",
-            "AppVersion": "102",
+            "AppVersion": "108",
             "Platform": "android",
             "GameId": "freefire",
             "SID": "1707129850016-19612",
             "TS": "some_value", // Previously "undefined", you should provide a meaningful value or remove if not needed
-            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiZ21pUHJvZmlsZUlkIjo5MzMzLCJleHAiOjE3MDczMDA5NzUsImZyZWVmaXJlUHJvZmlsZUlkIjo5MzMwLCJpYXQiOjE3MDcyMTQ1NzUsImlkIjo3MTE3fQ.w6hCMxZgpCUN1xtUwOGY-zRPyiV_2GMj_uKZxPL7PxI"
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJiZ21pUHJvZmlsZUlkIjo0Mzc5MjksImV4cCI6MTcwOTg4MjI4MSwiZnJlZWZpcmVQcm9maWxlSWQiOjQzNzkzOCwiaWF0IjoxNzA3MjkwMjgxLCJpZCI6NTA5NjIxfQ.A-RN9ieipztLIdcC6R-mOLnySHV9gozRdIC8EAmZFjs"
         }
     };
 
     const putPayload = {
-        "firebaseToken": "deNn_kL9RGKLg5STs0mgin:APA91bHYcAuV_KiTtYjWs2iKvYZuuwe3MpVc2A5tXoMDqxabgT-qlFlB18bL2B4Ja5zAWAASfEnACcmBM2mFCXJl9VNCoMfRCxAQ8sKRbh8kW_XRaJ9Rw33ATPe1_uqbb39kpy4c4dut"
+        
+            "username" : "Y0Z1OZAN",
+            "name": "Opgamer",
+            "bio": "Ghhfhgjghugugy ghgjhfhhhdjjjh gyvycycctxtxtxtctctxtxtuvuhdhd"
+        
     };
     
     urls.forEach(url => {
@@ -60,7 +63,7 @@ export default function () {
         }
 
         const isStatus200 = response.status === 200;
-        const isBodyNotEmpty = response.body.length > 0;
+        const isBodyNotEmpty = response.body ? response.body.length > 0 : false;
 
         const checkResult = check(response, {
             [`Status is 200 for ${url}`]: (r) => isStatus200,
