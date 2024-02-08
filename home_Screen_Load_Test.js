94dab2dd145c1f6caea8e50cd58a0321efdb540d
shashimadgi <145380337+shashimadgi@users.noreply.github.com>
2024-02-08 14:08:04 +0530

    Update home_Screen_Load_Test.js

--- home_Screen_Load_Test.js
+++ home_Screen_Load_Test.js
@@ -1,6 +1,15 @@
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
+
 import http from 'k6/http';
 import { check } from 'k6';
 
+export function handleSummary(data) {
+    return {
+      "result.html": htmlReport(data),
+      stdout: textSummary(data, { indent: " ", enableColors: true }),
+    };
+  }
 export const options = {
     vus: 50,
     duration: '10s',
